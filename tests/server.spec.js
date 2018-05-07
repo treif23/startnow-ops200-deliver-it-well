d99b874c8905b61340a3c5c419d8d06431d4e83b
Tommy <treif23@github.com>
2018-05-07 12:07:17 -0700

    update

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says is this working?', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('is this working?');
+        done();
+      });
+    });
 })
